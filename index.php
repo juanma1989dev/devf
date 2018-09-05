69373059f3f9b4cfe59d3b4ea1eae8779616a4a1
Juan Manuel Guzman <jmgr198810@gmail.com>
2018-09-04 20:54:27 -0500

    Se agrega el user agent

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "";
+$address = 'avenida+gustavo+paiva,maceio,alagoas,brasil';
+
+    $geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$address.'&sensor=false');
+
+    $output= json_decode($geocode);
+
+    $lat = $output->results[0]->geometry->location->lat;
+    $long = $output->results[0]->geometry->location->lng;
+
+    echo "Latitud :: {$lat} <br>";
+    echo "Longitud :: {$long}<br>";
+
+    echo "User Agent :: {$_SERVER['HTTP_USER_AGENT']}";
 
 ?>
